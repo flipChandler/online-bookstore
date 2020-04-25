061adfe029e3c13a888069259cffbf6fb5ea3003
Felipe Santos <felipe.santos11@hotmail.com>
2020-04-25 14:03:15 -0300

    alterar o uri do spring rest bookCategory -> book-category

--- onlinebookstore/src/main/java/br/com/felipesantos/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/br/com/felipesantos/onlinebookstore/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package br.com.felipesantos.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import br.com.felipesantos.onlinebookstore.entity.BookCategory;
 
+@RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
 
 }
